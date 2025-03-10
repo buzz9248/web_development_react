37892ba0b00ada0a44149865176edfe139ebb7f8
buzz9248 <buzz9248@naver.com>
2025-03-10 21:46:43 +0900

    feat: 20250310 shopping list add items function ended

---
+++ shoppinglist/src/components/Button.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function ClickButton() {
+  return (
+    <Button variant="contained" color="secondary">
+      클릭하세요
+    </Button>
+
+  );
+}
+
+export default ClickButton;
