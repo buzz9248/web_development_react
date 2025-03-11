5cfd0a3712db10a1ff3a6d4ad4af3c3cad61acf6
buzz9248 <buzz9248@naver.com>
2025-03-11 20:00:21 +0900

    feat:20250311 react router app started

--- shoppinglist/src/App2.tsx
+++ shoppinglist/src/App2.tsx
@@ -3,9 +3,15 @@ import Container from '@mui/material/Container'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import AddItem from './components/AddItem'
+// 쇼핑 항목을 표시하는 목록 관련 Component들 import
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
 
 import './App.css'
+
+import AddItem from './components/AddItem'
+
 
 export type Item = {
   product: string;
@@ -31,6 +37,19 @@ function App() {
           </Toolbar>
         </AppBar>
         <AddItem addItem={addItem} />
+
+        <List>
+          {
+            items.map((item, index) =>
+              <ListItem key={index} divider>
+                <ListItemText
+                  primary={item.product}
+                  secondary={item.amount} />
+              </ListItem>
+            )
+          }
+        </List>
+
       </Container>
     </>
   )
