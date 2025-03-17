d148e3dbc8cee226c6a252f27ae4da23d5b159e2
buzz9248 <buzz9248@naver.com>
2025-03-17 21:16:08 +0900

    feat: context API applied kpop list started

---
+++ kpoplist/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import Button from './Button';
+import KpopList from './KpopList';
+
+function Main({ data, playlist, setPlaylist }) {
+  return (
+    <>
+      <Button data={data} setPlaylist={setPlaylist} filterType="redvelvet">
+        🍎 레드벨벳 노래 찾기
+      </Button>
+      <Button data={data} setPlaylist={setPlaylist} filterType="male">
+        🍏 남자 아티스트 노래 찾기
+      </Button>
+      <Button data={data} setPlaylist={setPlaylist} filterType="female">
+        🍊 여자 아티스트 노래 찾기
+      </Button>
+      <KpopList playlist={playlist} />
+    </>
+  );
+}
+
+export default Main;
